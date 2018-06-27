c4ea4c02569eef65890309ed76cf9a2f1a4288cd
남정규 <namjkyou@naver.com>
2018-06-27 15:52:51 +0900

    sql_day02.sql 18.06.27 15:52

--- sql_day02.sql
+++ sql_day02.sql
@@ -318,3 +318,82 @@ SELECT e.empno
 ;
 
 --  2) 문자함수
+--  1. INITCAP(str) : str의 첫글자를 대문자로 변경(영문일 때)
+SELECT initcap('the soap') FROM dual; -- The Soap
+SELECT initcap('안녕하세요, 하하') FROM dual; -- 안녕하세요, 하하
+
+--  2. LOWER(str) : str을 소문자로 변경(영문일 때)
+SELECT lower('MR. SCOTT MCMILLAN') "소문자로 변경" FROM dual;
+
+--  3. UPPER(str) : str을 대문자로 변경(영문일 때)
+SELECT upper('sql is cooooooooooooooooool~!!') "씐나" FROM dual;
+
+--  4. LENGTH(str), LENGTHB(str) : str의 글자길이를 계산
+SELECT length('hello, sql') as "글자 길이" FROM dual;
+SELECT length('hello, sql의 글자 길이는') FROM dual;
+
+-- oracle에서 한글은 3byte로 계산
+SELECT lengthb('hello, sql') as "글자 byte" FROM dual;
+SELECT lengthb('오라클') as "글자 byte" FROM dual;
+
+--  5. CONCAT(str1, str2) : str1, str2 문자열을 접함, ||연산자와 동일
+SELECT concat('안녕하세요, ', 'SQL') FROM dual;
+SELECT '안녕하세요, ' || 'SQL' FROM dual;
+
+--  6. SUBSTR(str, i, n) : str에서 i번째 위치에서 n개의 글자를 추출
+--     SQL에서 문자열 인덱스를 나타내는 i는 1부터 시작(주의할 것)
+SELECT substr('sql is cooooooooooooooooool~!!', 3, 4) FROM dual;
+--     SUBSTR(str, i) : i번째 위치에서부터 문자열 끝까지 추출
+SELECT substr('sql is cooooooooooooooooool~!!', 3) FROM dual;
+
+--  7. INSTR(str1, str2) : 2번째 문자열이 1번째 문자열 어디에 위치하는지 계산
+SELECT instr('sql is cooooooooooooooooool~!!', 'is') FROM dual;
+SELECT instr('sql is cooooooooooooooooool~!!', 'ia') FROM dual;
+--     2번째 문자열이 없으면 0으로 계산
+
+--  8. LPAD, RPAD(str, n, c) : 입력된 str의 전체 자리수를 n으로 잡고 
+--                             남는 공간의 왼쪽/오른쪽에 문자 c를 채워넣는다.
+SELECT lpad('sql is cooool~!!', 20, '!') FROM dual;
+SELECT rpad('sql is cooool~!!', 25, '!') FROM dual; 
+
+--  9. LTRIM, RTRIM, TRIM : 입력된 문자열의 왼쪽, 오른쪽, 양쪽 공백 제거
+SELECT '>' || ltrim('      sql is cool     ') || '<' FROM dual;
+SELECT '>' || rtrim('      sql is cool     ') || '<' FROM dual;
+SELECT '>' || trim('      sql is cool     ') || '<' FROM dual;
+
+-- 10. NVL(expr1, expr2), NVL2(expr1, expr2, pxpr3), NULLIF(str1, str2)
+--     NVL(expr1, expr2) : expr1의 값이 NULL이면 expr2의 값 출력
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR, '매니저 없음') -- mgr값이 숫자, 출력값이 문자
+  FROM emp e                     -- 타입이 달라서 실행이 안됨
+;
+---------------------
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR, 0) 
+  FROM emp e                     
+;
+---------------------
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl(e.MGR || '', '매니저 없음') -- 숫자에 문자열을 덧붙이면 문자로 형변환
+  FROM emp e   
+;
+
+--     NVL2(expr1, expr2, pxpr3) : expr1의 값이 NOT NULL 이면 expr2의 값 출력, NULL이면 expr3의 값 출력
+SELECT e.EMPNO
+     , e.ENAME
+     , nvl2(e.MGR, '매니저 있음', '매니저 없음') 
+  FROM emp e                     
+;
+
+--     NULLIF(expr1, expr2) : expr1, expr2의 값이 같으면 NULL출력, 다르면 expr1의 값 출력
+SELECT nullif('AAA', 'BBB')
+  FROM dual
+;
+SELECT nullif('AAA', 'AAA')
+  FROM dual
+;
+-- 조회된 결과 1행이 NULL인 결과를 얻게 됨
+-- 1행이라도 NULL이 조회된 결과는 인출된 모든 행 : 0과는 상태가 다르다.
